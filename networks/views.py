e353a276e2aef8c2b0a7babf4f5cd715d98cf543
Mariya Khrustaleva <mbotmanova@gmail.com>
2026-01-31 22:20:36 +0500

    Add CRUD for network, contact, product models

--- networks/views.py
+++ networks/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from networks.models import Contact, Network, Product
+from networks.serializers import ContactSerializer, NetworkSerializer, ProductSerializer
+
+
+class NetworkViewSet(ModelViewSet):
+    queryset = Network.objects.all()
+    serializer_class = NetworkSerializer
+
+
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCreateAPIView(CreateAPIView):
+    queryset = Product.objects.all()
+
+
+class ProductUpdateAPIView(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDestroyAPIView(DestroyAPIView):
+    queryset = Product.objects.all()
+
+
+class ContactListAPIView(ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ContactDetailAPIView(RetrieveAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ContactCreateAPIView(CreateAPIView):
+    queryset = Contact.objects.all()
+
+
+class ContactUpdateAPIView(UpdateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ContactDestroyAPIView(DestroyAPIView):
+    queryset = Contact.objects.all()
